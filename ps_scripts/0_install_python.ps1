3238e9db9697b0c1ed14b8cee8574fed5239a210
Martin LD <tintamaria95@hotmail.fr>
2025-02-26 19:50:33 -0500

    removed mitmproxy from installation / corrected python installation

--- ps_scripts/0_install_python.ps1
+++ ps_scripts/0_install_python.ps1
@@ -1,26 +1,31 @@
 
-Write-Host "STEP 1/?: Download Python-3.11.6 installer..." -ForegroundColor Cyan
+Write-Host "STEP 1.1/3: Download Python-3.11.6 installer..." -ForegroundColor Cyan
 if (Test-Path $pythonInstaller) {
     Write-Host "-> python-installer.exe already downloaded."
 } else {
+    Write-Host "-> Downloading python-installer.exe."
     Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.6/python-3.11.6-amd64.exe" -OutFile $pythonInstaller
     if (-Not (Test-Path $pythonInstaller)) {
-        Write-Host "-> Download failed. EXIT SCRIPT" -ForegroundColor Red
+        Write-Host "-> Download failed." -ForegroundColor Red
         exit 1
     }
     Write-Host "-> Download succeeded."
 }
 
 
-Write-Host "STEP2/?: Python installation" -ForegroundColor Cyan
-Start-Process -FilePath $pythonInstaller -ArgumentList "/quiet InstallAllUsers=0 TargetDir=$pythonDir PrependPath=0" -Wait
-# TODO add Get-Command to check installation was really successful, if not add advice to repair python or script to do so
-Write-Host "-> Local Python installation completed in $pythonDir"
+Write-Host "STEP 1.2/3:: Installing Python. Please wait and do not close this window..." -ForegroundColor Cyan
+Start-Process -FilePath $pythonInstaller -ArgumentList "/quiet InstallAllUsers=0 TargetDir=`"$pythonDir`" PrependPath=0" -Wait
 
-Write-Host "STEP ?/?: Upgrading pip" -ForegroundColor Cyan
-& $pythonExe -m pip install --upgrade pip --no-warn-script-location
+if ( -Not(Get-Command $pythonExe -ErrorAction SilentlyContinue)){
+    Write-Host "Python installation did not succeed, please use GUI and select `"Repair Python`""
+    Start-Process -FilePath $pythonInstaller -ArgumentList "Repair"  -Wait
+} 
+
+if ( Get-Command $pythonExe -ErrorAction SilentlyContinue){
+    Write-Host "-> Local Python installation completed in $pythonDir"
+} else {
+    Write-Host "-> Local Python installation did not complete in $pythonDir"
+    exit 1
+}
 
 
-Write-Host "STEP ?/?: Installing python requirements..." -ForegroundColor Cyan
-$requirementsPath = [System.IO.Path]::Combine($currFolderPath, "..", "requirements.txt")
-& $pythonExe -m pip install -r requirements.txt  --no-warn-script-location
